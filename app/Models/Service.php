64798f55f40ef88690a3232e47435523c9566ef8
arifincoding <arifinnakpojok@gmail.com>
2022-06-11 19:44:32 +0700

    change model service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model{
     protected $fillable = [
-        'nama','kategori','keluhan','cacatProduk','kelengkapan','catatan','uangMuka','status','estimasiBiaya','idCustomer','butuhKonfirmasi','dikonfirmasi','konfirmasiBiaya','diambil','tanggalMasuk','tanggalAmbil','jamAmbil','jamMasuk','usernameCS','usernameTeknisi','totalBiaya'
+        'kode','keluhan','uangMuka','status','estimasiBiaya','idCustomer','idProduct','butuhPersetujuan','disetujui','konfirmasiBiaya','diambil','waktuMasuk','waktuAmbil','usernameCS','usernameTeknisi','totalBiaya','garansi'
     ];
 }
 
