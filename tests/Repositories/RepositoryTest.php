cb0863c2e18a6e8f1117106fb91807d44141db2d
arifincoding <arifinnakpojok@gmail.com>
2022-10-27 12:45:06 +0700

    test repo

--- tests/Repositories/RepositoryTest.php
+++ tests/Repositories/RepositoryTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Category;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 
 class RepositoryTest extends TestCase
@@ -12,32 +13,54 @@ class RepositoryTest extends TestCase
     {
         parent::setUp();
         $this->repository = $this->app->make('App\Repositories\CategoryRepository');
+        $this->category = Category::factory()->count(3)->create();
     }
 
-    public function testShouldCreateAData()
+    public function testShouldCreateSingleData()
     {
         $result = $this->repository->save(['nama' => 'uji create single category']);
-        $this->assertEquals([1, 'uji create single category'], [$result->id, $result->nama]);
+        $this->assertEquals([4, 'uji create single category'], [$result->id, $result->nama]);
     }
 
-    public function testShouldUpdateData()
+    public function testShouldUpdateSingleDataById()
     {
-        Category::factory()->count(3)->create();
         $result = $this->repository->save(['nama' => 'uji update single category'], 2);
         $this->assertEquals([2, 'uji update single category'], [$result->id, $result->nama]);
     }
 
     public function testShouldFindDataById()
     {
-        $category = Category::factory()->count(3)->create();
         $result = $this->repository->findById(2);
-        $this->assertEquals($category[1]->toArray(), $result->toArray());
+        $this->assertEquals($this->category[1]->toArray(), $result->toArray());
     }
 
-    public function testShoudDeleteDataById()
+    public function testFindDataByIdShouldReturnException()
     {
-        Category::factory()->count(3)->create();
+        $this->expectException(ModelNotFoundException::class);
+        $this->repository->findById(4);
+    }
+
+    public function testFindDataByIdShouldReturnNull()
+    {
+        $result = $this->repository->findById(4, ['*'], false);
+        $this->assertEquals($result, null);
+    }
+
+    public function testShoudDeleteSingleDataById()
+    {
         $result = $this->repository->delete(2);
         $this->assertEquals($result, true);
+    }
+
+    public function testDeleteSingleDataByIdShouldReturnException()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $this->repository->delete(4);
+    }
+
+    public function testDeleteSingleDataByIdShouldReturnFalse()
+    {
+        $result = $this->repository->delete(4, 'id', false);
+        $this->assertEquals($result, false);
     }
 }
