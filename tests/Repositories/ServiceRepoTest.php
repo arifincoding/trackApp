0aa6f1c9e3cf89a983ba9cf34d82e137c4ddc473
arifincoding <arifinnakpojok@gmail.com>
2023-04-04 17:40:08 +0700

    update service repository test

--- tests/Repositories/ServiceRepoTest.php
+++ tests/Repositories/ServiceRepoTest.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Models\Broken;
-use App\Models\Category;
 use App\Models\Customer;
 use App\Models\History;
 use App\Models\Product;
+use App\Models\Responbility;
 use App\Models\Service;
+use App\Models\User;
 use Illuminate\Support\Carbon;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
@@ -24,103 +25,125 @@ class ServiceRepoTest extends TestCase
 
     public function testShouldGetListService()
     {
-        $this->markTestSkipped();
-        // $customer = Customer::factory()->count(3)->create();
-        // $product = Product::factory()->count(3)->create();
-        // foreach ($product as $key => $item) {
-        // Service::factory()->count($key + 1)->for($customer[$key], 'client')->for($product[$key], 'product')->create();
-        // }
-        $result = $this->repository->getListData(['status' => 'tunggu']);
-        // $service = Service::with('client', 'product')->orderByDesc('id')->get();
-        // $this->assertEquals($service->toArray(), $result->toArray());
-        // $this->assertEquals($service[0]->client->toArray(), $result[0]->client->toArray());
-        // $this->assertEquals($service[0]->product->toArray(), $result[0]->product->toArray());
-        // echo $result[0]->customers->name;
-        echo json_encode($result->toArray());
+
+        Service::factory()->count(3)->create();
+
+        $attributs = [
+            'services.id as service_id',
+            'services.code',
+            'services.complaint',
+            'services.status',
+            'services.total_cost',
+            'services.is_take',
+            'services.is_approved',
+            'customers.name as customer_name',
+            'customers.telp',
+            'products.name as products_name',
+            'categories.name as category'
+        ];
+
+        $service = Service::select($attributs)->join('products', 'services.product_id', 'products.id')->join('customers', 'products.customer_id', 'customers.id')->join('categories', 'products.category_id', 'categories.id')->orderByDesc('services.id')->get();
+
+        $result = $this->repository->getListData();
+
+        $this->assertEquals($service->toArray(), $result->toArray());
     }
 
     public function testShouldGetSingleServiceByIdWithCustomerProductAndBrokens()
     {
-        $this->markTestSkipped();
-        $customer = Customer::factory()->count(3)->create();
-        $product = Product::factory()->count(3)->create();
-        $i = 1;
-        foreach ($customer as $key => $item) {
-            Service::factory()->for($customer[$key], 'client')->for($product[$key], 'product')->has(Broken::factory()->count($i++), 'broken')->create();
-        }
-        $service = Service::with(['client', 'product', 'broken' => function ($q) {
+
+        $serviceFactory = Service::factory()->create();
+
+        Broken::factory()->count(4)->create(['service_id' => $serviceFactory->id]);
+
+        $result = $this->repository->getDataWithRelationById($serviceFactory->id);
+
+        $service = Service::with(['product', 'product.client', 'product.category', 'broken' => function ($q) {
             $q->orderByDesc('id');
-        }])->where('id', 2)->first();
-        $result = $this->repository->getDataWithRelationById(2);
+        }])->where('id', $serviceFactory->id)->first();
+
         $this->assertEquals($service->toArray(), $result->toArray());
-        $this->assertEquals($service->client->toArray(), $result->client->toArray());
-        $this->assertEquals($service->product->toArray(), $result->product->toArray());
-        $this->assertEquals($service->broken->toArray(), $result->broken->toArray());
     }
 
     public function testShouldGetListServiceQueue()
     {
-        $this->markTestSkipped();
-        // $category = Category::factory()->count(7)->create();
-        // $status = ['antri', 'proses', 'antri', 'antri', 'antri', 'selesai', 'antri'];
-        // Product::factory()->count(7)->sequence(function ($sequence) use ($category) {
-        //     return ['category' => $category[$sequence->index]->name];
-        // })->create();
-        // Service::factory()->count(7)->sequence(function ($sequence) use ($status) {
-        //     return ['product_id' => $sequence->index + 1, 'status' => $status[$sequence->index]];
-        // });
-        // $responbility = [];
-        // $j = 0;
-        // for ($i = 2; $i < 5; $i++) {
-        //     $responbility[$j++]['category'] = ['name' => $category[$i]->id];
-        // }
-        // $service = Service::with('product')->whereIn('id', [3, 4, 5])->orderByDesc('id')->get();
-        $result = $this->repository->getListDataQueue('30031999', ['search' => 'window']);
-        echo json_encode($result->toArray());
-        // $this->assertEquals($service->toArray(), $result->toArray());
+
+        $serviceFactory = Service::factory()->count(3)->create(['tecnician_username' => null, 'status' => 'antri']);
+
+        $attributs = [
+            'services.id as service_id',
+            'services.code',
+            'services.complaint',
+            'services.status',
+            'services.is_approved',
+            'products.name as product_name',
+            'categories.name as category'
+        ];
+
+        $user = User::factory()->create(['role' => 'teknisi', 'username' => '30031999']);
+
+        $product = Product::whereIn('id', [$serviceFactory[0]->product_id, $serviceFactory[1]->product_id])->get();
+
+        Responbility::factory()->count(2)->sequence(['category_id' => $product[0]->category_id, 'username' => $user->username], ['category_id' => $product[1]->category_id, 'username' => $user->username])->create();
+
+        $service = Service::select($attributs)->join('products', 'services.product_id', 'products.id')->join('categories', 'products.category_id', 'categories.id')->whereIn('services.id', [$serviceFactory[0]->id, $serviceFactory[1]->id])->orderByDesc('services.id')->get();
+
+        $result = $this->repository->getListDataQueue($user->username);
+
+        $this->assertEquals($service->toArray(), $result->toArray());
     }
 
     public function testShouldGetListProgressService()
     {
-        // $this->markTestSkipped();
-        // $usernames = ['2211001', '2211002', '2211003'];
-        // foreach ($usernames as $key => $username) {
-        // $product = Product::factory()->create();
-        // Service::factory()->count(3)->for($product, 'product')->create(['tecnician_username' => $username]);
-        // }
-        // $service = Service::with('product')->whereIn('id', [4, 5, 6])->orderByDesc('id')->get();
 
-        $result = $this->repository->getListDataMyProgress('30031999', ['search' => 'mati', 'category' => 'motor', 'status' => 'antri']);
-        echo json_encode($result->toArray());
-        // $this->assertEquals($service->toArray(), $result->toArray());
+        $user = User::factory()->create(['role' => 'teknisi', 'username' => '30031999']);
+
+        $service = Service::factory()->count(5)->sequence(
+            ['status' => 'mulai diagnosa'],
+            ['tecnician_username' => $user->username, 'status' => 'selesai diagnosa'],
+            ['tecnician_username' => $user->username, 'status' => 'selesai'],
+            ['status' => 'tunggu'],
+            ['tecnician_username' => $user->username, 'status' => 'selesai']
+        )->create();
+
+        $attributs = [
+            'services.id as service_id',
+            'services.code',
+            'services.complaint',
+            'services.status',
+            'services.is_approved',
+            'products.name as product_name',
+            'categories.name as category'
+        ];
+
+        $service = Service::select($attributs)->join('products', 'services.product_id', 'products.id')->join('categories', 'products.category_id', 'categories.id')->whereIn('services.id', [$service[1]->id, $service[2]->id, $service[4]->id])->orderByDesc('services.id')->get();
+
+        $result = $this->repository->getListDataMyProgress('30031999');
+
+        $this->assertEquals($service->toArray(), $result->toArray());
     }
 
     public function testShouldgetDataByCode()
     {
-        $this->markTestSkipped();
-        $code = ['221108001', '221108002', '221108003'];
-        $products = Product::factory()->count(3)->create();
-        foreach ($products as $key => $product) {
-            Service::factory()->for($product, 'product')->has(Broken::factory()->count(3), 'broken')->has(History::factory()->count(4), 'history')->create(['code' => $code[$key]]);
-        }
-        $service = Service::with(['product', 'broken' => function ($q) {
+        $serviceFactory = Service::factory()->count(3)->create();
+        Broken::factory()->count(4)->create(['service_id' => $serviceFactory[1]->id]);
+        History::factory()->count(4)->create(['service_id' => $serviceFactory[1]->id]);
+        $service = Service::with(['product', 'product.category', 'broken' => function ($q) {
             $q->orderByDesc('id');
         }, 'history' => function ($q) {
             $q->orderByDesc('id');
-        }])->where('code', '221108002')->first();
-        $result = $this->repository->getDataByCode('221108002');
-
+        }])->where('id', $serviceFactory[1]->id)->first();
+        $result = $this->repository->getDataByCode($serviceFactory[1]->code);
         $this->assertEquals($service->toArray(), $result->toArray());
     }
 
     public function testShouldSetCodeServiceInSingleServiceDataById()
     {
-        $this->markTestSkipped();
+        $serviceFactory = Service::factory()->count(3)->create(['code' => null]);
         $date = Carbon::now('GMT+7');
-        $code = $date->format('y') . $date->format('m') . $date->format('d') . sprintf("%03d", 2);
-        Service::factory()->count(3)->create(['code' => null]);
-        $result = $this->repository->setCodeService(2);
-        $service = Service::find(2);
+        $code = $date->format('y') . $date->format('m') . $date->format('d') . sprintf("%03d", $serviceFactory[1]->id);
+        $result = $this->repository->setCodeService($serviceFactory[1]->id);
+        $service = Service::find($serviceFactory[1]->id);
         $this->assertEquals(true, $result);
         $this->assertEquals($code, $service->code);
     }
