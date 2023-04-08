b9543fa0e067373f0112758a13dd35d14484414a
arifincoding <arifinnakpojok@gmail.com>
2023-04-08 16:46:33 +0700

    updated factory

--- database/factories/BrokenFactory.php
+++ database/factories/BrokenFactory.php
@@ -13,13 +13,22 @@ class BrokenFactory extends Factory
 
     public function definition()
     {
-        $service = Service::factory()->create();
         return [
             'title' => $this->faker->sentence(3),
             'is_approved' => null,
             'description' => $this->faker->paragraph(2),
-            'service_id' => $service->id,
+            'service_id' => null,
             'cost' => $this->faker->numberBetween(25000, 1000000)
         ];
+    }
+
+    public function withRelation(): Factory
+    {
+        $service = Service::factory()->create();
+        return $this->state(function (array $attributes) use ($service) {
+            return [
+                'service_id' => $service->id
+            ];
+        });
     }
 }
--- database/factories/ResponbilityFactory.php
+++ database/factories/ResponbilityFactory.php
@@ -15,11 +15,31 @@ class ResponbilityFactory extends Factory
 
     public function definition()
     {
+        return [
+            'username' => '',
+            'category_id' => ''
+        ];
+    }
+
+    public function withRelation(): Factory
+    {
         $user = User::factory()->create();
         $category = Category::factory()->create();
-        return [
-            'username' => $user->username,
-            'category_id' => $category->id
-        ];
+        return $this->state(function (array $attributes) use ($user, $category) {
+            return [
+                'username' => $user->username,
+                'category_id' => $category->id
+            ];
+        });
+    }
+
+    public function withCategory(): Factory
+    {
+        $category = Category::factory()->create();
+        return $this->state(function (array $attributes) use ($category) {
+            return [
+                'category_id' => $category->id
+            ];
+        });
     }
 }
